cf3e9ad752c4e5414068eedcf876e31764d8672b
suTCS <j.parksu@tcs.com>
2018-02-19 02:51:58 -0500

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TodoProvider } from '../providers/todo/todo';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TodoProvider
   ]
 })
 export class AppModule {}
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,117 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, reorderArray, ToastController } from 'ionic-angular';
+import { TodoProvider } from '../../providers/todo/todo';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public todos = [];
+  public reorderIsEnabled = false;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController,
+    private todoService: TodoProvider, private toastCtrl: ToastController) {
+      this.todos = this.todoService.getTodos();
   }
 
+  openTodoAlert() {
+    let addTodoAlert = this.alertCtrl.create({
+      title: "Add a Todo",
+      subTitle: "Add a Todo list one by one",
+      message: "Enter your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput",
+          placeholder: "Add Todo"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+        {
+          text: "Add",
+          handler: (inputData) => {
+            let todoText = inputData.addTodoInput;
+            this.todoService.addTodo(todoText);
+
+              addTodoAlert.onDidDismiss(()=> {  //display toast after added todo list
+                let toast = this.toastCtrl.create({
+                  message: 'Todo Added',
+                  duration: 3000,
+                  position: 'middle'
+                });
+                toast.onDidDismiss(() => {
+                  console.log('Dismissed toast');
+                });
+                toast.present();
+              })
+
+          }
+        }
+      ]
+    });
+    addTodoAlert.present();
+  }
+
+  toggleReorder() {
+    this.reorderIsEnabled = !this.reorderIsEnabled;
+  }
+
+  reorderItems(indexes) {
+    this.todos = reorderArray(this.todos, indexes);
+  }
+
+  // gotoArchivePage() {
+  //   this.navCtrl.push('ArchivedTodosPage');
+  // }
+
+  deleteTodo(index) {
+    this.todoService.deletetodo(index);
+  }
+
+  edittodoo(index) {
+    let editTodoAlert = this.alertCtrl.create({
+      title: "Edit a Todo",
+      subTitle: "Edit a Todo list",
+      message: "Edit your Todo",
+      inputs: [
+        {
+          type: "text",
+          name: "editTodoInput",
+          value: this.todos[index] //preset the value w/ current value
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+        {
+          text: "Save",
+          handler: (inputData) => {
+            let todoText = inputData.editTodoInput;
+            this.todoService.editTodo(todoText, index);
+
+              editTodoAlert.onDidDismiss(()=> {  //display toast after edited todo list
+                let toast = this.toastCtrl.create({
+                  message: 'Todo Edited',
+                  duration: 3000,
+                  position: 'bottom'
+                });
+                toast.onDidDismiss(() => {
+                  console.log('Dismissed toast');
+                });
+                toast.present();
+              })
+
+          }
+        }
+      ]
+    });
+    editTodoAlert.present();
+  }
 }
